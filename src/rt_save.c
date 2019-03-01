3099728bb8eaffb83543a5643c8fb8bd0ff9209a
Dr-Haas <garysaah@gmail.com>
2019-03-01 18:44:21 +0100

    rt_save protected

--- src/rt_save.c
+++ src/rt_save.c
@@ -1,50 +1,73 @@
 
 #include "../rt.h"
 #include "debug.h"
+#include "libft_convert.h"
 
 void		rt_save(int fd)
 {
 	size_t 			i;
 	t_object* 		object;
-//	t_primitive 	primitive = NULL;
-	char*			type;
-	char*			material;
-	char*			color;
-	char*			pos;
-	char*			rot;
-	char*			scale;
+	char*			tmp;
 
 	i = 0;
+	if ((tmp = FT_U32_To_HexString(rt.scene.bg_color)))
+	{	
+		FT_Write_String(fd, "BG #");
+		FT_Write_Line(fd, tmp);
+		FT_Write_Char(fd, '\n');
+	}
 	while (i < rt.scene.object_amount)
 	{
 		object = &rt.scene.objects[i];
 		if (object)
 		{
-			type = rt_get_str_primitive(object->type);
-			material = rt_get_str_material(object->material);
-			color = cl_float3_to_str(&object->rgb, 5);
-			pos = cl_float3_to_str(&object->pos, 5);
-			rot = cl_float3_to_str(&object->rot, 5);
-			scale = cl_float3_to_str(&object->scale, 5);
-
-			FT_Write_Line(fd, type);
-			FT_Write_String(fd,"material:");
-			FT_Write_Line(fd, material);
-			FT_Write_String(fd, "color:");
-			FT_Write_Line(fd, color);
-			FT_Write_String(fd, "pos:");
-			FT_Write_Line(fd, pos);
-			FT_Write_String(fd, "rot:");
-			FT_Write_Line(fd, rot);
-			FT_Write_String(fd, "scale:");
-			FT_Write_Line(fd, scale);
+			
+			if ((tmp = rt_get_str_primitive(object->type)))
+			{
+				FT_Write_Line(fd, tmp);
+		
+			}
+			if ((tmp = rt_get_str_material(object->material)))
+			{
+				FT_Write_String(fd,"material:");
+				FT_Write_Line(fd, tmp);
+			}
+			if ((tmp = cl_float3_to_str(&object->rgb, 5)))
+			{
+				FT_Write_String(fd, "color:");
+				FT_Write_Line(fd, tmp);
+				free(tmp);
+			}
+			if ((tmp = cl_float3_to_str(&object->pos, 5)))
+			{
+				FT_Write_String(fd, "pos:");
+				FT_Write_Line(fd, tmp);
+				free(tmp);
+			}
+			if ((tmp = cl_float3_to_str(&object->rot, 5)))
+			{
+				FT_Write_String(fd, "rot:");
+				FT_Write_Line(fd, tmp);
+				free(tmp);
+			}
+			if ((tmp = cl_float3_to_str(&object->scale, 5)))
+			{
+				FT_Write_String(fd, "scale:");
+				FT_Write_Line(fd, tmp);
+				free(tmp);
+			}
+			if (ft_strlen(object->name))
+			{
+				FT_Write_Char(fd, '"');
+				FT_Write_String(fd, object->name);
+				FT_Write_Char(fd, '"');
+				FT_Write_Char(fd, '\n');
+			}
 			FT_Write_Char(fd, '\n');
+
 		}
 		i++;
 	}
-	free(color);
-	free(pos);
-	free(rot);
-	free(scale);
+	FT_Write_Char(fd, '\n');
 
 }

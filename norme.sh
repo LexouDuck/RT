31cf6146b78d29e2a846cafef464a34da7b37125
Lexou <lexou-dit-tuc@hotmail.fr>
2019-03-13 00:41:51 +0100

    added norme script

---
+++ norme.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+norminette src/*.c > norme_c.txt
+norminette src/*.h > norme_h.txt
+norminette *.h >> norme_h.txt
+norminette libft/src/*/*.c norme_libft_c.txt
+norminette libft/*.h > norme_libft_h.txt

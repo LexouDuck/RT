1a2be32b9222b19a8acc5304c7e759a60eb49d63
hbruvry <hugo.bruvry@gmail.com>
2019-03-12 20:55:07 +0100

    Norme rt_open_read_materials

--- src/rt_open_read_materials.c
+++ src/rt_open_read_materials.c
@@ -15,31 +15,39 @@
 #include "libft_convert.h"
 #include "../rt.h"
 
+void		rt_get_arg_color_rgb(char c, t_rtparser *p, cl_float3 *result)
+{
+	char	*str;
+	t_u32	color;
+
+	++(p->index);
+	str = p->file + p->index;
+	while ((c = p->file[p->index]) && (ft_isdigit(c) ||
+		('A' <= c && c <= 'F') || ('a' <= c && c <= 'f')))
+		++(p->index);
+	if (p->file[p->index] == '\n')
+		p->line += 1;
+	p->file[p->index] = '\0';
+	++(p->index);
+	color = ft_hex_to_u32(str);
+	result->x = ft_color_argb32_get_r(color) / 255.;
+	result->y = ft_color_argb32_get_g(color) / 255.;
+	result->z = ft_color_argb32_get_b(color) / 255.;
+	return ;
+}
+
 char		*rt_read_arg_color(t_rtparser *p, cl_float3 *result,
 			char const *label)
 {
 	char	c;
 	char	*str;
-	t_u32	color;
 
 	rt_read_whitespace(p);
 	if ((c = p->file[p->index]) == '\0')
 		return (NULL);
 	else if (c == '#')
 	{
-		++(p->index);
-		str = p->file + p->index;
-		while ((c = p->file[p->index]) && (ft_isdigit(c) ||
-			('A' <= c && c <= 'F') || ('a' <= c && c <= 'f')))
-			++(p->index);
-		if (p->file[p->index] == '\n')
-			p->line += 1;
-		p->file[p->index] = '\0';
-		++(p->index);
-		color = ft_hex_to_u32(str);
-		result->x = ft_color_argb32_get_r(color) / 255.;
-		result->y = ft_color_argb32_get_g(color) / 255.;
-		result->z = ft_color_argb32_get_b(color) / 255.;
+		rt_get_arg_color_rgb(c, p, result);
 		return (NULL);
 	}
 	else
@@ -55,19 +63,12 @@ char		*rt_read_arg_color(t_rtparser *p, cl_float3 *result,
 	}
 }
 
-char		*rt_read_arg_material(t_rtparser *p, t_material *result, char const *label)
+t_bool		rt_check_arg_material_label(t_rtparser *p, t_material *result)
 {
 	char const	*str;
 	size_t		length;
 	size_t		i;
 
-	rt_read_whitespace(p);
-	if (!p->file[p->index] || !ft_strnequ(p->file + p->index, label, ft_strlen(label)))
-		return (NULL);
-	p->index += ft_strlen(label);
-	if (p->file[p->index] != ':')
-		return (rt_read_error(':', "without spaces before material string", p->file[p->index]));
-	++(p->index);
 	i = 0;
 	while (i < MATERIALS)
 	{
@@ -77,9 +78,30 @@ char		*rt_read_arg_material(t_rtparser *p, t_material *result, char const *label
 		{
 			*result = (t_material)i;
 			p->index += length;
-			return (NULL);
+			return (TRUE);
 		}
 		++i;
 	}
+	return (FALSE);
+}
+
+char		*rt_read_arg_material(t_rtparser *p, t_material *result,
+			char const *label)
+{
+	rt_read_whitespace(p);
+	if (!p->file[p->index] || !ft_strnequ(p->file + p->index, 
+	label, ft_strlen(label)))
+	{
+		return (NULL);
+	}
+	p->index += ft_strlen(label);
+	if (p->file[p->index] != ':')
+	{
+		return (rt_read_error(':', "without spaces before material string",
+		p->file[p->index]));
+	}
+	++(p->index);
+	if(rt_check_arg_material_label(p, result))
+		return (NULL);
 	return ("No valid material enum label encountered");
 }
